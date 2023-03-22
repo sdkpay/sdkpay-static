c3d9353007dd448824cea70d5244e09f8d133667
AlexanderIpatov <AlexanderIpatov1997@gmail.com>
2023-03-22 13:08:28 +0300

    rename package

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "PaySDK",
+    name: "SberPaySDK",
     platforms: [
         .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PaySDK",
-            targets: ["PaySDK"]),
+            name: "SberPaySDK",
+            targets: ["SberPaySDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
