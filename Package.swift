a50be34d74fa1c076039473aa6b8e5202d706a38
AlexanderIpatov <AlexanderIpatov1997@gmail.com>
2023-03-22 12:56:43 +0300

    add first ver

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "PaySDK",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +21,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "PaySDK",
-            dependencies: []),
-        .testTarget(
-            name: "PaySDKTests",
-            dependencies: ["PaySDK"]),
+        .binaryTarget(name: "SberPaySDK",
+                      path: "SberPaySDK.xcframework")
     ]
 )
--- Sources/PaySDK/PaySDK.swift
+++ Sources/PaySDK/PaySDK.swift
@@ -1,6 +1,4 @@
 public struct PaySDK {
-    public private(set) var text = "Hello, World!"
-
     public init() {
     }
 }
