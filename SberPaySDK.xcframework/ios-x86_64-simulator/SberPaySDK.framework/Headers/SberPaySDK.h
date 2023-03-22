e280852ad68e56aad6c8c0926846c19bd56bb9b4
AlexIpatov <123178101+sdkpay@users.noreply.github.com>
2023-03-22 20:16:02 +0300

    build change

---
+++ SberPaySDK.xcframework/ios-x86_64-simulator/SberPaySDK.framework/Headers/SberPaySDK.h
@@ -0,0 +1,18 @@
+//
+//  SberPaySDK.h
+//  SberPaySDK
+//
+//  Created by Alexander Ipatov on 09.11.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SberPaySDK.
+FOUNDATION_EXPORT double SberPaySDKVersionNumber;
+
+//! Project version string for SberPaySDK.
+FOUNDATION_EXPORT const unsigned char SberPaySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SberPaySDK/PublicHeader.h>
+
+
