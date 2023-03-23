5ca96e36aed6e136c76bbaca007b54094ad3c60b
AlexIpatov <123178101+sdkpay@users.noreply.github.com>
2023-03-23 15:17:34 +0300

    fix for package

--- Package.swift
+++ Package.swift
@@ -19,9 +19,13 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "SberPaySDK",
-                      path: "SberPaySDK.xcframework")
+                      path: "SberPaySDK.xcframework"),
+        .target(name: "SberPaySDK",
+                dependencies: [
+                    .target(name: "SberPaySDK")
+                ],
+                path: "./"
+    )
     ]
 )
